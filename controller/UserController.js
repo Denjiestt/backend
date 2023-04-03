0200fba2cbcd0688f7b933be509db0204f4d7229
Denjiestt <-v>
2023-04-03 23:56:44 +0700

    menambahkan sesuatu 123

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,4 +1,4 @@
-import User from '../models/Usermodel.js'
+import User from '../models/UserModel.js'
 import argon2 from "argon2"
 import jwt from "jsonwebtoken"
 import nodemailer from "nodemailer"
